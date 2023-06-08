35a55529c9b4fd9f3405a52809ebb30bccc4c53a
SY-ML <ai.engr.sy@gmail.com>
2023-06-08 23:09:37 +0900

    060823

--- stock.py
+++ stock.py
@@ -85,9 +85,6 @@ for com_name, com_ticker in dict_ticker.items():
     dict_data[com_name] = sp.data
 
 
-print(dict_data)
-
-exit()
 # Define the ticker symbol
 tickerSymbol = 'MVST'
 
@@ -101,66 +98,67 @@ data = data.history(period='1d', start='2019-1-1', end='2023-12-31')
 data_input = data.drop('High', axis=1)
 data_target = data['High']
 
-print(data_input)
-print(data_target)
-exit()
-# Scale the input data to [0, 1] range
-scaler = MinMaxScaler(feature_range=(0, 1))
-data_input_scaled = scaler.fit_transform(data_input)
 
-# Split the dataset into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(data_input_scaled, data_target, test_size=0.2, random_state=42)
+def process_dataset_for_tensorflow(dataset):
+    # Scale the input data to [0, 1] range
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    data_input_scaled = scaler.fit_transform(dataset)
 
-# Reshape input to be 3D [samples, timesteps, features] for LSTM
-X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
-X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
+    # Split the dataset into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(data_input_scaled, data_target, test_size=0.2, random_state=42)
 
-# Define the LSTM model
-model = Sequential()
-model.add(LSTM(50, input_shape=(X_train.shape[1], X_train.shape[2])))
-model.add(Dense(1))
-model.compile(loss='mae', optimizer=Adam())
+    # Reshape input to be 3D [samples, timesteps, features] for LSTM
+    X_train = X_train.reshape((X_train.shape[0], 1, X_train.shape[1]))
+    X_test = X_test.reshape((X_test.shape[0], 1, X_test.shape[1]))
 
-# Create callback for early stopping on validation loss. If the loss does not decrease in
-# two consecutive tries, stop training
-early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+    return (X_train, X_test, y_train, y_test)
 
-# Create model checkpoint
-checkpoint = ModelCheckpoint('model.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+def build_LSTM_model(dataset):
+    X_train, X_test, y_train, y_test = dataset
+    # Define the LSTM model
+    model = Sequential()
+    model.add(LSTM(50, input_shape=(X_train.shape[1], X_train.shape[2])))
+    model.add(Dense(1))
+    model.compile(loss='mae', optimizer=Adam())
 
-# Fit the LSTM model
-model.fit(
-    X_train,
-    y_train,
-    epochs=100,
-    batch_size=72,
-    verbose=0,
-    validation_split=0.2,  # set validation split for early stopping
-    callbacks=[early_stopping, checkpoint]
-)
+    # Create callback for early stopping on validation loss. If the loss does not decrease in
+    # two consecutive tries, stop training
+    early_stopping = EarlyStopping(monitor='val_loss', patience=2)
 
-import matplotlib.pyplot as plt
+    # Create model checkpoint
+    checkpoint = ModelCheckpoint('model.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 
-# Fit the LSTM model
-history = model.fit(
-    X_train,
-    y_train,
-    epochs=100,
-    batch_size=72,
-    verbose=0,
-    validation_split=0.2,  # set validation split for early stopping
-    callbacks=[early_stopping, checkpoint]
-)
+    # Fit the LSTM model
+    model.fit(
+        X_train,
+        y_train,
+        epochs=100,
+        batch_size=72,
+        verbose=0,
+        validation_split=0.2,  # set validation split for early stopping
+        callbacks=[early_stopping, checkpoint]
+    )
 
-# Plot the training and validation loss
-plt.figure(figsize=(10, 6))
+    # Fit the LSTM model
+    history = model.fit(
+        X_train,
+        y_train,
+        epochs=100,
+        batch_size=72,
+        verbose=0,
+        validation_split=0.2,  # set validation split for early stopping
+        callbacks=[early_stopping, checkpoint]
+    )
 
-plt.plot(history.history['loss'], label='Training Loss')
-plt.plot(history.history['val_loss'], label='Validation Loss')
+    # Plot the training and validation loss
+    plt.figure(figsize=(10, 6))
 
-plt.title('Model Loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
 
-plt.legend(loc='upper right')
-plt.show()
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+
+    plt.legend(loc='upper right')
+    plt.show()
